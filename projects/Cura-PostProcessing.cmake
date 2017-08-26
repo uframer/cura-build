b6300968b529e762861b042d22f35af0add8ea74
Jiao Ye <uframer@gmail.com>
2017-08-26 17:41:33 +0800

    Use uframer-PostProcessingPlugin instead of upstream repository
    Though I haven't modified this repository yet, I do require PostProcessingPlugin
    repository to contain a tag named `uframer-2.5.0`.

--- projects/Cura-PostProcessing.cmake
+++ projects/Cura-PostProcessing.cmake
@@ -1,6 +1,6 @@
 
 ExternalProject_Add(Cura-PostProcessing
-    GIT_REPOSITORY https://github.com/nallath/PostProcessingPlugin
+    GIT_REPOSITORY https://github.com/uframer/uframer-PostProcessingPlugin
     GIT_TAG origin/${TAG_OR_BRANCH}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
     UPDATE_DISCONNECTED ${WORK_OFFLINE}
